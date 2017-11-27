ce1e26850070c5537119e2d03061b206c094b30b
Margrét ósk Rannversdóttir <margretoskrannversdottir@Margrets-MacBook-Air.local>
2017-11-27 13:46:05 +0000

    hallo thelma

---
+++ Assingm 1/Assingm 1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assingm 1
+//
+//  Created by Margrét ósk Rannversdóttir on 27/11/2017.
+//  Copyright © 2017 Margrét ósk Rannversdóttir. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
