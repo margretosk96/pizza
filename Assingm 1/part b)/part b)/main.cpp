49ae3719c271c610b644f33f0eb3283cc9c6bada
Thelma Rut Jóhannsdóttir <thelmarut@Thelmas-MacBook-Pro.local>
2017-11-28 13:48:21 +0000

    changes to part a and a little in part b

--- Assingm 1/part b)/part b)/main.cpp
+++ Assingm 1/part b)/part b)/main.cpp
@@ -12,29 +12,6 @@ using namespace std;
 
 int main() {
   
-    string str;
-    ifstream fin;
-    fin.open("NewDataTextFile.txt");
-    if(fin.is_open()) {
-        //*while(!fin.eof()){
-        
-            fin >> str;
-            cout << str << endl;
-        
-        }
-        //*cout << endl << endl;
-        cout << fin.eof() << endl;
-    
-    
-    else {
-    cout << "File not open" << endl;
-
-}
-    ofstream fout;
-    fout.open("NewDataTextFile.txt", ios::app);
-    fout << str << endl;
-    fin.close();
-    
     
     
     return 0;
